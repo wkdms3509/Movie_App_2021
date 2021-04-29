2ee4136cba03bdb05a736e896007dacf912f651b
samantha <lattechoi2@gmail.com>
2021-04-29 19:11:00 +0900

    axios 설치 및 Fetching movies api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div>
-      <h2>hello</h2>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true
+  };
+
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json")
+  }
+
+  componentDidMount() {
+    this.getMovies()
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <div>
+        {isLoading ? "Loading..." : "We are ready"}
+      </div>
+    )
+  }
 }
 
 export default App;
